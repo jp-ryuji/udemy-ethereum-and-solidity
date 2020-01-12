707c69dd8346a7b9707dcd1e768959c74545527b
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-12 15:17:01 +0900

    45. Refactor to Async/Await

--- section2/test/Inbox.test.js
+++ section2/test/Inbox.test.js
@@ -2,3 +2,19 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');  // NOTE: Web3 は constructor function
 const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy the contract
+
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
+});
