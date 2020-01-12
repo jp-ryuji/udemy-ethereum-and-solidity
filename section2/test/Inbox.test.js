5330cf25b7cd5ea9f7788c5b055e75a3ddf7ac97
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-12 15:36:31 +0900

    49. Web3 Version Fix

--- section2/test/Inbox.test.js
+++ section2/test/Inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');  // NOTE: Web3 は constructor function
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -15,6 +16,8 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
