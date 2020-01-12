76315e6b849ab93064268b8274f9d7eaff6439c9
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-13 06:29:55 +0900

    The whole code for the section3

---
+++ section3/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+// NOTE: The followings are exactly what're used in the sample repo.
+const provider = new HDWalletProvider(
+  'game saddle oyster laundry equal loop lunch allow cactus endless hover unfair',
+  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  // This is for truffle-hdwallet-provider version 0.0.3
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  // This is for truffle-hdwallet-provider versions 0.0.4, 0.0.5 and 0.0.6
+  // const result = await new web3.eth.Contract(JSON.parse(interface))
+  //    .deploy({ data: '0x' + bytecode }) // add 0x bytecode
+  //    .send({ from: accounts[0] }); // remove 'gas'
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
