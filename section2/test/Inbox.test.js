e100c7678939cffe9d38df3069be6e7e71ef5265
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-12 14:26:43 +0900

    41. Web3 Providers

--- section2/test/Inbox.test.js
+++ section2/test/Inbox.test.js
@@ -1,3 +1,4 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');  // NOTE: Web3 は constructor function
+const web3 = new Web3(ganache.provider());
