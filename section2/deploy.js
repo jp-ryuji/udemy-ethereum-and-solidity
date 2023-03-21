d352d8522b20087c0e8d104036544335b7b9e0d5
Ryuji Eguchi <jp.ryuji@gmail.com>
2023-03-21 17:19:48 +1100

    section2 done (with solidity and other npm modules update as written at the last lesson of the section)

--- section2/deploy.js
+++ section2/deploy.js
@@ -1,29 +1,27 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const Web3 = require("web3")
+require("dotenv").config()
 
-// NOTE: The followings are exactly what're used in the sample repo.
+const { abi, evm } = require("./compile")
+
 const provider = new HDWalletProvider(
-  'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
-  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
-);
-const web3 = new Web3(provider);
+  process.env.MNEMONIC,
+  process.env.GOERLI_ENDPOINT_WITH_API_KEY,
+)
+
+const web3 = new Web3(provider)
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts()
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0])
 
-  // This is for truffle-hdwallet-provider version 0.0.3
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] })
 
-  // This is for truffle-hdwallet-provider versions 0.0.4, 0.0.5 and 0.0.6
-  // const result = await new web3.eth.Contract(JSON.parse(interface))
-  //    .deploy({ data: '0x' + bytecode, arguments: ['Hi there!'] }) // add 0x bytecode
-  //    .send({ from: accounts[0] }); // remove 'gas'
+  console.log("Contract deployed to", result.options.address)
+  provider.engine.stop()
+}
 
-  console.log('Contract deployed to', result.options.address);
-};
-deploy();
+deploy()
