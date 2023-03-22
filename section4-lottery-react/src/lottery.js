da09d08e02c4da6864d024b3ec93d0d81dcbb559
Ryuji Eguchi <jp.ryuji@gmail.com>
2023-03-22 11:21:24 +1100

    update lottery.js

--- section4-lottery-react/src/lottery.js
+++ section4-lottery-react/src/lottery.js
@@ -1,59 +1,60 @@
-import web3 from './web3';
+import web3 from "./web3"
 
-const address = '0x71A85f1DCbEfed3C6DC20457D26d79aaF9A1F69e';
+const address = "0xe2e857d1b120C78A46BEa780fB4c72Ce1584c209"
+
 const abi = [
   {
     inputs: [],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
-    signature: 'constructor',
+    stateMutability: "nonpayable",
+    type: "constructor",
+    signature: "constructor",
   },
   {
     inputs: [],
-    name: 'enter',
+    name: "enter",
     outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
+    stateMutability: "payable",
+    type: "function",
     payable: true,
-    signature: '0xe97dcb62',
+    signature: "0xe97dcb62",
   },
   {
     inputs: [],
-    name: 'getPlayers',
+    name: "getPlayers",
     outputs: [
-      { internalType: 'address payable[]', name: '', type: 'address[]' },
+      { internalType: "address payable[]", name: "", type: "address[]" },
     ],
-    stateMutability: 'view',
-    type: 'function',
+    stateMutability: "view",
+    type: "function",
     constant: true,
-    signature: '0x8b5b9ccc',
+    signature: "0x8b5b9ccc",
   },
   {
     inputs: [],
-    name: 'manager',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function',
+    name: "manager",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
     constant: true,
-    signature: '0x481c6a75',
+    signature: "0x481c6a75",
   },
   {
     inputs: [],
-    name: 'pickWinner',
+    name: "pickWinner",
     outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-    signature: '0x5d495aea',
+    stateMutability: "nonpayable",
+    type: "function",
+    signature: "0x5d495aea",
   },
   {
-    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    name: 'players',
-    outputs: [{ internalType: 'address payable', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function',
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ internalType: "address payable", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
     constant: true,
-    signature: '0xf71d96cb',
+    signature: "0xf71d96cb",
   },
-];
+]
 
-export default new web3.eth.Contract(abi, address);
+export default new web3.eth.Contract(abi, address)
