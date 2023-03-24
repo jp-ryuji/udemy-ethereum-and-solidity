70139b030c83bc397d3358677e302c617e24e70b
Ryuji Eguchi <jp.ryuji@gmail.com>
2023-03-24 16:56:14 +1100

    replace Campaign.sol with one with solidity 0.8.19

--- section6-kickstart/ethereum/contracts/Campaign.sol
+++ section6-kickstart/ethereum/contracts/Campaign.sol
@@ -1,80 +1,100 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 contract CampaignFactory {
-    address[] public deployedCampaigns;
+  address[] public deployedCampaigns;
 
-    function createCampaign(uint minimum) public {
-        address newCampaign = new Campaign(minimum, msg.sender);
-        deployedCampaigns.push(newCampaign);
-    }
+  function createCampaign(uint256 minimum) public {
+    Campaign newCampaign = new Campaign(minimum, msg.sender);
+    deployedCampaigns.push(address(newCampaign));
+  }
 
-    function getDeployedCampaigns() public view returns (address[]) {
-        return deployedCampaigns;
-    }
+  function getDeployedCampaigns() public view returns (address[] memory) {
+    return deployedCampaigns;
+  }
 }
 
 contract Campaign {
-    struct Request {
-        string description;
-        uint value;
-        address recipient;
-        bool complete;
-        uint approvalCount;
-        mapping(address => bool) approvals;
-    }
+  struct Request {
+    string description;
+    uint256 value;
+    address payable recipient;
+    bool complete;
+    uint256 approvalCount;
+    mapping(address => bool) approvals;
+  }
 
-    Request[] public requests;
-    address public manager;
-    uint public minimumContribution;
-    mapping(address => bool) public approvers;
-    uint public approversCount;
+  Request[] public requests;
+  address public manager;
+  uint public minimumContribution;
+  mapping(address => bool) public approvers;
+  uint public approversCount;
 
-    modifier restricted() {
-        require(msg.sender == manager);
-        _;
-    }
+  modifier restricted() {
+    require(msg.sender == manager);
+    _;
+  }
 
-    function Campaign(uint minimum, address creator) public {
-        manager = creator;
-        minimumContribution = minimum;
-    }
+  constructor(uint256 minimum, address creator) {
+    manager = creator;
+    minimumContribution = minimum;
+  }
 
-    function contribute() public payable {
-        require(msg.value > minimumContribution);
+  function contribute() public payable {
+    require(msg.value > minimumContribution, "The amount sent is less than the minimum contribution");
 
-        approvers[msg.sender] = true;
-        approversCount++;
-    }
+    approvers[msg.sender] = true;
+    approversCount++;
+  }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
-        Request memory newRequest = Request({
-           description: description,
-           value: value,
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
-        });
+  function createRequest(
+    string calldata description,
+    uint256 value,
+    address payable recipient
+  ) public restricted
+  {
+    Request storage newRequest = requests.push();
+    newRequest.description = description;
+    newRequest.value = value;
+    newRequest.recipient = recipient;
+    newRequest.complete = false;
+    newRequest.approvalCount = 0;
 
-        requests.push(newRequest);
-    }
+    // This error is thrown with the succeeding code:
+    //   from solidity: TypeError: Struct containing a (nested) mapping cannot be constructed.
+    //
+    // Revised as above by referencing: https://stackoverflow.com/questions/63170366/solidity-solc-error-struct-containing-a-nested-mapping-cannot-be-constructed
+    //
+    // Request storage newRequest = Request({
+    //   description: description,
+    //   value: value,
+    //   recipient: recipient,
+    //   complete: false,
+    //   approvalCount: 0
+    // });
+    //
+    // requests.push(newRequest);
+  }
 
-    function approveRequest(uint index) public {
-        Request storage request = requests[index];
+  function approveRequest(uint256 index) public {
+    Request storage request = requests[index]; // `storage` is used to reference `requests[index]` instead of copying the value
 
-        require(approvers[msg.sender]);
-        require(!request.approvals[msg.sender]);
+    require(approvers[msg.sender], "Only contributors can approve requests");
+    require(!request.approvals[msg.sender], "The contributor has already approved this request");
 
-        request.approvals[msg.sender] = true;
-        request.approvalCount++;
-    }
+    request.approvals[msg.sender] = true;
+    request.approvalCount++;
+  }
 
-    function finalizeRequest(uint index) public restricted {
-        Request storage request = requests[index];
+  function finalizeRequest(uint256 index) public restricted {
+    Request storage request = requests[index];
 
-        require(request.approvalCount > (approversCount / 2));
-        require(!request.complete);
+    require(!request.complete);
+    require(request.approvalCount > (approversCount / 2));
 
-        request.recipient.transfer(request.value);
-        request.complete = true;
-    }
+    (bool sent, ) = request.recipient.call{value: request.value}("");
+    require(sent, "Failed to send Ether");
+
+    request.complete = true;
+  }
 }
