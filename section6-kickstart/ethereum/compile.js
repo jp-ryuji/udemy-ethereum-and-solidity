d9ede179c3e7dfa24e59d1e03a7cb5c3efaf00b6
Ryuji Eguchi <jp.ryuji@gmail.com>
2023-03-24 18:01:35 +1100

    revise compile.js (for solc 0.8.x)

--- section6-kickstart/ethereum/compile.js
+++ section6-kickstart/ethereum/compile.js
@@ -2,19 +2,51 @@ const path = require("path")
 const solc = require("solc")
 const fs = require("fs-extra")
 
+const srcFileName = "Campaign.sol"
+
 const buildPath = path.resolve(__dirname, "build")
 fs.removeSync(buildPath)
 
-const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol")
+const campaignPath = path.resolve(__dirname, "contracts", srcFileName)
 const source = fs.readFileSync(campaignPath, "utf8")
 // CampaignFactory and Campaign are returned. They are processed using a for-loop in the following step.
-const output = solc.compile(source, 1).contracts
+// const output = solc.compile(source, 1).contracts
+
+// fs.ensureDirSync(buildPath)
+
+// for (let contract in output) {
+//   fs.outputJsonSync(
+//     path.resolve(buildPath, contract.replace(":", "") + ".json"),
+//     output[contract],
+//   )
+// }
+
+// revised version for solc 0.8.19
+// ref: https://ethereum.stackexchange.com/questions/125616/how-to-complie-smart-contract-in-node-js-solc-version-0-8-13
+const input = {
+  language: "Solidity",
+  sources: {
+    "Campaign.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+}
+
+// compile contract
+const output = JSON.parse(solc.compile(JSON.stringify(input)))
 
 fs.ensureDirSync(buildPath)
 
-for (let contract in output) {
+for (let contract in output.contracts[srcFileName]) {
   fs.outputJsonSync(
-    path.resolve(buildPath, contract.replace(":", "") + ".json"),
-    output[contract],
+    path.resolve(buildPath, contract + ".json"),
+    output.contracts[srcFileName][contract].evm,
   )
 }
