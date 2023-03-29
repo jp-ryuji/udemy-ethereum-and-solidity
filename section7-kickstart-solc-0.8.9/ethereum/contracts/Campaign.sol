b06c761c55ffc4214949ab56abe8cba664a3969d
Ryuji Eguchi <jp.ryuji@gmail.com>
2023-03-29 13:20:12 +1100

    just add spaces before =

--- section7-kickstart-solc-0.8.9/ethereum/contracts/Campaign.sol
+++ section7-kickstart-solc-0.8.9/ethereum/contracts/Campaign.sol
@@ -51,10 +51,10 @@ contract Campaign {
     function createRequest(string memory description, uint value, address recipient) public restricted {
         Request storage newRequest = requests.push();
         newRequest.description = description;
-        newRequest.value= value;
-        newRequest.recipient= recipient;
-        newRequest.complete= false;
-        newRequest.approvalCount= 0;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
     }
 
     function approveRequest(uint index) public {
